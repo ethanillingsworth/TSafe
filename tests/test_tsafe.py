71b4151bba006e61e87586a62e3c0245035e4210
Ethan Illingsworth <illingsworth.ethan@gmail.com>
2024-10-18 16:52:57 -0500

    update tests

--- tests/test_tsafe.py
+++ tests/test_tsafe.py
@@ -1,4 +1,9 @@
-
+from tsafe import safe, StringList
 
 def test_empty():
-    assert None == None
+    
+    @safe
+    def random(s: str, n: int, n2: float, l: list, d: dict, a: object, sl: StringList):
+        assert True
+
+    random("String", 10, 1.123, ["types", 10, "multiple"], {"Hello": 10}, "Anything", ["Strings", "Only!!"])
