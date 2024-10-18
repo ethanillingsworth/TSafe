2bef8bf814f343a7f96a24177bda04e8c7550502
Ethan Illingsworth <illingsworth.ethan@gmail.com>
2024-10-18 16:47:50 -0500

    Simplify force_safety and fix issues with class methods not working

--- tsafe/__init__.py
+++ tsafe/__init__.py
@@ -36,34 +36,35 @@ def type_safe(func):
             for index in range(0, len(argType)):
                 arg = argType[index]
 
-                try:
-                    reqType = list(func.__annotations__.values())[index]
-                except IndexError:
+                if classCheck:
                     reqType = list(func.__annotations__.values())[index - 1]
+                else:
+                    reqType = list(func.__annotations__.values())[index]
+
 
                 # skip self if in class
                 if func.__qualname__ and 'self' in signature(func).parameters and not classCheck:
                     classCheck = True
                     continue
+                    
+                listTypes = [StringList, IntList, FloatList, ComplexList, BoolList]
 
-                if type(arg) == list:
-                    listTypes = [StringList, IntList, FloatList, ComplexList, BoolList]
-                    for l in listTypes:
-                        
-                        if (reqType == l and all(type(x) == l.REQTYPE for x in arg)) or reqType == list:
-                            flag = True
-                            break
-                        else:
-                            flag = False
-
-                    if not flag:
-                        raise Exception(f"argument {arg} is not type of {reqType}")
-                        break
-
-                elif type(arg) == reqType or reqType == object:
+                if type(arg) == reqType or reqType == object:
                     continue
                 else:
                     flag = False
+                    
+
+                for l in listTypes:
+                    
+                    if (reqType == l and all(type(x) == l.REQTYPE for x in arg)) or reqType == list:
+                        flag = True
+                        break
+                    else:
+                        flag = False
+                
+
+                if not flag:
                     raise Exception(f"argument {arg} is not type of {reqType}")
                     break
 
@@ -73,3 +74,14 @@ def type_safe(func):
     return force_safety
 
 safe = type_safe
+
+class Hello():
+    @safe
+    def __init__(self, x: int):
+        print(x)
+
+    @safe
+    def hello(self, b: StringList, c: bool=True):
+        print(1)
+
+Hello(10).hello(["world"], False)
